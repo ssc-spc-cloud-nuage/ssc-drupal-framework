a56ab4be00ad675218c21236e6534b744ac3c75a
Peter Lindstrom <peter@liquidcms.ca>
2024-07-31 12:11:07 -0400

    fix translation in time ago twig filter

--- modules/common/ssc_common/src/Twig/SSCTwig.php
+++ modules/common/ssc_common/src/Twig/SSCTwig.php
@@ -2,11 +2,20 @@
 
 namespace Drupal\ssc_common\Twig;
 
+use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
 class SSCTwig extends AbstractExtension {
+  use StringTranslationTrait;
+
+  protected DateFormatterInterface $dateFormatter;
+
+  public function __construct(DateFormatterInterface $date_formatter) {
+    $this->dateFormatter = $date_formatter;
+  }
 
   public function getFilters() {
     return [
@@ -28,14 +37,36 @@ class SSCTwig extends AbstractExtension {
   public function timeAgo($text) {
     $timestamp = $text->__toString();
     if ($timestamp > time()) {
-      $time_until = \Drupal::service('date.formatter')
-        ->formatTimeDiffUntil($timestamp, ['granularity' => 1]);
-      return new TranslatableMarkup('in @interval', ['@interval' => $time_until]);
+      $time_until = $this->dateFormatter->formatTimeDiffUntil($timestamp, ['granularity' => 1]);
+      $time_until_translated = $this->translateTimeInterval($time_until);
+      return new TranslatableMarkup('in @interval', ['@interval' => $time_until_translated]);
     }
     else {
-      $time_since = \Drupal::service('date.formatter')
-        ->formatTimeDiffSince($timestamp,  ['granularity' => 1]);
-      return new TranslatableMarkup('@interval ago', ['@interval' => $time_since]);
+      $time_since = $this->dateFormatter->formatTimeDiffSince($timestamp, ['granularity' => 1]);
+      $time_since_translated = $this->translateTimeInterval($time_since);
+      return new TranslatableMarkup('@interval ago', ['@interval' => $time_since_translated]);
     }
   }
+
+  protected function translateTimeInterval($time_interval) {
+    if (preg_match('/(\d+)\s+(\w+)/', $time_interval, $matches)) {
+      $number = $matches[1];
+      $unit = $matches[2];
+
+      // Translate the unit
+      $translated_unit = new TranslatableMarkup($unit);
+
+      // Use formatPlural for proper translation
+      return \Drupal::translation()->formatPlural(
+        $number,
+        '1 @unit',
+        '@count @unit',
+        ['@unit' => $translated_unit]
+      );
+    } else {
+      return $time_interval; // Fallback if parsing fails
+    }
+  }
+
+
 }
