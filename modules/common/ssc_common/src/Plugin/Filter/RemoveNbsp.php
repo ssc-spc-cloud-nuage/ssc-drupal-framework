6748c71f61ffad77fb95e249a616ee3eb1431c63
Serena Zhu <serenazhu0223@gmail.com>
2024-08-27 13:20:15 -0400

    update remove nbsp filter

--- modules/common/ssc_common/src/Plugin/Filter/RemoveNbsp.php
+++ modules/common/ssc_common/src/Plugin/Filter/RemoveNbsp.php
@@ -4,6 +4,7 @@ namespace Drupal\ssc_common\Plugin\Filter;
 
 use Drupal\filter\FilterProcessResult;
 use Drupal\filter\Plugin\FilterBase;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Delete all &nbsp; and whitespace from TDs and DIVs.
@@ -14,6 +15,9 @@ use Drupal\filter\Plugin\FilterBase;
  *   title = @Translation("Remove NBSP"),
  *   description = @Translation("Remove NBSP and whitespace from empty TDs and DIVs."),
  *   type = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_IRREVERSIBLE,
+ *   settings = {
+ *     "tags" = "td div"
+ *   },
  * )
  */
 class RemoveNbsp extends FilterBase {
@@ -40,8 +44,23 @@ class RemoveNbsp extends FilterBase {
     return implode(' ', $tips);
   }
 
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $form['tags'] = [
+      '#type' => 'textfield',
+      '#title' => t('Tags'),
+      '#default_value' => $this->settings['tags'] ?? 'td div',
+      '#required' => TRUE,
+      '#description' => t('Tags to remove whitespace, separate each tag with space.'),
+    ];
+
+    return $form;
+  }
+
   private function removeNBSP($text) {
-    $pattern = '/<(td|div)>\s*&nbsp;\s*<\/\1>|<(td|div)>\s*<\/\1>/i';
+    $values = $this->settings['tags'];
+    $array_tags = explode(' ', $values);
+    $tags = implode('|', $array_tags);
+    $pattern = '/<(' . $tags . ')>\s*&nbsp;\s*<\/\1>|<(' . $tags . ')>\s*<\/\1>/i';
     $replacement = '<$1></$1>';
     return preg_replace($pattern, $replacement, $text);
   }
