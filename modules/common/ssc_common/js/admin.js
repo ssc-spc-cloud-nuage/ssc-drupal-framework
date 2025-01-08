4a8f2b3835f12649d76d6019ec1567178962396e
Peter Lindstrom <peter@liquidcms.ca>
2025-01-08 15:27:07 -0500

    sg-318: fixes for FR event form

--- modules/common/ssc_common/js/admin.js
+++ modules/common/ssc_common/js/admin.js
@@ -8,13 +8,6 @@
 
   Drupal.behaviors.sscCommonAdmin = {};
   Drupal.behaviors.sscCommonAdmin.attach = function (context, settings) {
-    // Improve Session fieldset labels on Event edit form - NOTE: needs to re-run after Ajax
-    $('[id^="edit-field-sessions"] > [data-drupal-selector^="edit-field-sessions-widget"]').each((function () {
-      const date = $(this).find('input.form-date').attr('value');
-      const lang = $(this).find('div.field--name-field-session-language select option:selected').text();
-      $(this).find('summary').first().html(date + " - " + lang);
-    }));
-
     // Hide Country selector on custom address for Session - painful to do with CSS
     $('#node-event-form label:contains("Country")').parent().hide();
 
@@ -22,7 +15,7 @@
       // Add form-required to the Start date input of the Campaign date field
       $('.field--name-field-campaign-date .fieldset__label').addClass('form-required');
       $('.field--name-field-campaign-date h4.form-item__label:contains("Start")').addClass('form-required');
-      
+
       // Move Node form buttons to bottom of Meta group
       $('form.node-form #edit-meta').append($('#edit-actions'));
     });
