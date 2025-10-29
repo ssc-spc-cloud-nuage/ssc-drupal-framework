88032bbf4afb3c3ec913556478f2b9d925c29abe
Mikaël Sévigny <sevignator@hotmail.com>
2025-10-29 09:03:36 -0400

    Add utility classes and default styling

--- themes/common/ssc_base/js/global.js
+++ themes/common/ssc_base/js/global.js
@@ -6,11 +6,43 @@
 (function ($, Drupal, once) {
   "use strict";
 
-  Drupal.behaviors.sscBase = {};
-  Drupal.behaviors.sscBase.attach = function (context, settings) {
-    once('ssc-base', document.documentElement, context).forEach(function () {
-      // Add stretched link class to upcoming events teasers on the News landing page
-      $(".upcoming-event a").addClass("stretched-link");
-    });
+  Drupal.behaviors.sscBase = {
+    attach: (context, settings) => {
+      once("ssc-base", document.documentElement, context).forEach(function () {
+        // Add stretched link class to upcoming events teasers on the News landing page
+        $(".upcoming-event a").addClass("stretched-link");
+
+        // Remove the scrollbar's width from the total width of `container-full-width`
+        let scrollbarWidth = 0;
+        $(document).ready(function () {
+          const observer = new ResizeObserver((entries) => {
+            const newScrollbarWidth =
+              window.innerWidth - document.documentElement.clientWidth;
+
+            if (newScrollbarWidth === scrollbarWidth) return;
+
+            scrollbarWidth = newScrollbarWidth;
+
+            document.documentElement.style.setProperty(
+              "--size-scrollbar",
+              `${scrollbarWidth}px`
+            );
+          });
+
+          observer.observe(document.documentElement);
+        });
+
+        // Apply background image added via the `data-image-url` data attribute
+        $(document).ready(function () {
+          document.querySelectorAll(".bg-image").forEach((el) => {
+            const imageUrl = el.getAttribute("data-image-url");
+
+            if (imageUrl) {
+              el.style.backgroundImage = `url('${imageUrl}')`;
+            }
+          });
+        });
+      });
+    },
   };
 })(jQuery, Drupal, once);
